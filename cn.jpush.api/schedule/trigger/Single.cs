00560b581842265c61a4e6de962c39fedfe04b65
fendouai <fendouai@gmail.com>
2016-03-18 14:06:23 +0800

    fix

--- cn.jpush.api/schedule/trigger/Single.cs
+++ cn.jpush.api/schedule/trigger/Single.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using cn.jpush.api.util;
+
 namespace cn.jpush.api.schedule
 {
     public class Single
@@ -12,7 +14,7 @@ namespace cn.jpush.api.schedule
 
         public void setTime(string time)
         {
-       
+            Preconditions.checkArgument(StringUtil.IsDateTime(time),"the time is not valid");
             this.time = time;
         }
 
