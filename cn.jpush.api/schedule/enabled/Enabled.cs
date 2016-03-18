37af273f0ce6cb982f6c0de6dc4b88ccab73a869
fendouai <fendouai@gmail.com>
2016-03-18 16:26:31 +0800

    add enable test

--- cn.jpush.api/schedule/enabled/Enabled.cs
+++ cn.jpush.api/schedule/enabled/Enabled.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using cn.jpush.api.util;
 
 namespace cn.jpush.api.schedule
 {
@@ -10,8 +11,8 @@ namespace cn.jpush.api.schedule
     {
         private bool enable;
 
-        public void setEnable(bool enable) {
-            this.enable = enable;
+        public void setEnable(bool enable) { 
+            this.enable = enable;  
         }
         public bool getEnable()
         {
